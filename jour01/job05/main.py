5e90ea0b4d925a6f8e85049d5c578b8d9abc6807
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-03 10:20:05 +0100

    Added job05

---
+++ jour01/job05/main.py
@@ -0,0 +1,3 @@
+#Job 5 : Hey back, i don't know why some have used a loop to do this, but (ngl) this is completly useless.
+
+print("z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a")
