2c2d1b3dd265854fbecc7a87877d0af0db0c7ce6
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-03 10:18:11 +0100

    Added job04

---
+++ jour01/job04/main.py
@@ -0,0 +1,3 @@
+#Job 4 : Hello there, welcome back. I decided that i have to put space between all the letters, this is why i printed every letter one by one.
+
+print("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
