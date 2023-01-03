c6ab95eae19c23010c43b02061d4d64ab160784b
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-03 10:34:24 +0100

    Added pour_aller_plus_loin0000000000000000000000000000000000000000000000

---
+++ jour01/pour_aller_plus_loins/main.py
@@ -0,0 +1,7 @@
+#... pour aller plus loin. OH, hello there, i... didn't see you walk in. I was busy watching random unprotected security cameras
+
+your_string = "PUT YOUR STRING HERE"
+if "e" in your_string:
+    print('il existe bien un "e" ou plus dans votre chaine de charactères.')
+else:
+    print("il n'y a pas de 'e' dans votre chaine de charactères")
