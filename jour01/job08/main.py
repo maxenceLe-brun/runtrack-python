22c6c0fd04c4b3e7f586a0cbcdf91a09bcc5c798
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-03 10:30:59 +0100

    Added job08

---
+++ jour01/job08/main.py
@@ -0,0 +1,4 @@
+#Job 8 : SAME AS BEFORE, but this time, the window is open and i can now watch the outside world of my mind prison (don't worry, this is just my mental health)
+num1 = 3
+num2 = 14
+print(num1 * num2)
