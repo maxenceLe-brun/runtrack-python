332bdea5ed5306fb8cf432895e32a2ff2f746e4b
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-03 10:16:05 +0100

    Added Job03

---
+++ jour01/job03/main.py
@@ -0,0 +1,5 @@
+#Job 3 : Here, i'll talking about my python setup. I'm used to code with Spyder since 5 years, but i'm familiar with Pycharm too (never ask me to work on VScode)
+
+    #My IDE : Spyder
+    #My Python version : Py3.9.7
+    
