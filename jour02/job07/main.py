78fb94d15330eb66afe4f5680f392fce7a4b7748
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-04 10:29:54 +0100

    Create main.py

---
+++ jour02/job07/main.py
@@ -0,0 +1,7 @@
+#Job 7 : So we've just finished to play the "Squidward mode" and instead move on enriching activities, such as hurricane siren.
+def yourSkills(langage):
+    if langage == "javascript":print("tu es un developpeur web")
+    elif langage == "python":print("tu es un developpeur IA")
+    elif langage == "java":print("tu es un developpeur logiciel")
+    elif langage == "reactjs": print("tu es un developpeur mobile")
+    else:print("un jour je serai le meilleur developpeur...")
