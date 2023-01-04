cb4b00c99511c40831a1f8ac57adae3f0c3b7dc1
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-04 10:25:26 +0100

    Added job06

---
+++ jour02/job06/main.py
@@ -0,0 +1,11 @@
+#Job 6 : I'm a big fan of the Horror gameplay
+def position(nombre):
+    if nombre>0:
+        print("positif")
+    elif nombre<0:
+        print("negatif")
+    else:
+        print("nul")
+position(5)
+position(0.3)
+position(-0.0001)
