4276b26e596c0ee62f3ac380ce067f817403fea5
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-04 10:32:45 +0100

    Added job08

---
+++ jour02/job8/main.py
@@ -0,0 +1,6 @@
+#Job 8 : this is a foreshadowing : skill issue 
+def Job08(type,saison):
+    if type == "fruits" and saison == "hiver":print("orange, mandarine et kiwi")
+    if type == "fruits" and saison == "ete":print("Poire, fraise, cassis")
+    if type == "legume" and saison == "hiver":print("carotte, topinamboure, endive")
+    if type == "legume" and saison == "ete":print("artichaut, aubergine, navet")
