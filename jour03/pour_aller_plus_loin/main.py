5c20c21a80d69601dc4f1e528b555aceac7f5c9c
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-05 10:28:38 +0100

    Create main.py

---
+++ jour03/pour_aller_plus_loin/main.py
@@ -0,0 +1,9 @@
+#... pour aller plus loin. Oh sht watch out i'm comming through
+def reverse(word):
+    liste = list(word)
+    liste.reverse()
+    result = ""
+    for a in liste:
+        result += a
+    print(result)
+        
