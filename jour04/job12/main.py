a798fb817982a819ca6b8afc0c1f7d54fb37fd4c
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-01-06 10:33:01 +0100

    Create main.py

---
+++ jour04/job12/main.py
@@ -0,0 +1,20 @@
+#Job 12 :
+L = []
+for a in liste:
+    L += [a]
+result = []
+while L != []:
+    MIN = L[0]
+    for a in L:
+        if a < MIN:
+            MIN = a
+        else:
+            print(a)
+    l = []
+    for b in L:
+        if MIN == b:
+            result += [MIN]
+        else:
+            l += [b]
+    L = l
+print(result)
